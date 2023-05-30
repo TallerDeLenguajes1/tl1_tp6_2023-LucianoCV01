5529d785f1e665dab41832a9be3d7ba2b72e4de9
LucianoCV01 <Lucianocosentino01@gmail.com>
2023-05-30 19:04:28 -0300

    Ejercicio 3, Terminado

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -30,6 +30,26 @@
         //     }
         // }
 
+        //------ Ejercicio 3 ------
+        float num1, num2, num3;
+        bool resultado2, resultado3;
+
+        Console.Write("Ingrese un numero: ");
+        if (float.TryParse(Console.ReadLine(), out num1))
+        {
+            calculadoraMejorada(num1);
+        }
+        Console.Write("Ingrese un numero: ");
+        resultado2 = float.TryParse(Console.ReadLine(), out num2);
+        Console.Write("Ingrese un numero: ");
+        resultado3 = float.TryParse(Console.ReadLine(), out num3);
+        if(resultado2 && resultado3)
+        {
+            Console.WriteLine("Numeros: " + num2 + ", " + num3);
+            Console.WriteLine("Maximo: " + maxNumero(num2, num3));
+            Console.WriteLine("Minimo: " + minNumero(num2, num3));
+        }
+
     }
     public static string invertirNumero(int num)
     {
@@ -41,4 +61,38 @@
             return (num%10).ToString() + invertirNumero(num/10);
         }
     }
+
+    public static float maxNumero(float num1, float num2)
+    {
+        if (num1 == num2 || num1 > num2)
+        {
+            return num1;
+        } else
+        {
+            return num2;
+        }
+    }
+
+    public static float minNumero(float num1, float num2)
+    {
+        if (num1 == num2 || num1 < num2)
+        {
+            return num1;
+        } else
+        {
+            return num2;
+        }
+    }
+
+    public static void calculadoraMejorada(float num)
+    {
+        Console.WriteLine("Numero: " + num);
+        Console.WriteLine("Valor Absoluto: " + Math.Abs(num));
+        Console.WriteLine("Cuadradro: " + (float)Math.Pow(num, 2));
+        Console.WriteLine("Raiz cuadrada: " + (float)Math.Sqrt(num));
+        Console.WriteLine("Seno: " + (float)Math.Sin(num));
+        Console.WriteLine("Coseno: " + (float)Math.Cos(num));
+        Console.WriteLine("Parte entera de un tipo float: " + (int)num);
+    }
+
 }
