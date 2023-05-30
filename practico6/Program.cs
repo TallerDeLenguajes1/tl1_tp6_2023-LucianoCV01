b53ef8a40234604974d320c78bf4148779b6b029
LucianoCV01 <Lucianocosentino01@gmail.com>
2023-05-29 22:44:22 -0300

    Ejercicios Propuestos, Terminados

---
+++ practico6/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        /* Ejercicios Propuestos */
+        //Apartado 6
+        Console.WriteLine("Hello, World!");
+        int a;
+        int b;
+        a=10;
+        b=a;
+        Console.WriteLine("valor de a:"+a);
+        Console.WriteLine("valor de b:"+b);
+    }
+}
