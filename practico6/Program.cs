9352fc8b3c007b7a0719048e9c47809a7b618633
LucianoCV01 <Lucianocosentino01@gmail.com>
2023-05-30 01:06:27 -0300

    Ejercicio 1, Terminado

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -4,14 +4,41 @@
     {
         Console.WriteLine("Hello, World!");
 
-        /* Ejercicios Propuestos */
+        //------ Ejercicios Propuestos ------
         //Apartado 6
-        Console.WriteLine("Hello, World!");
-        int a;
-        int b;
-        a=10;
-        b=a;
-        Console.WriteLine("valor de a:"+a);
-        Console.WriteLine("valor de b:"+b);
+        // Console.WriteLine("Hello, World!");
+        // int a;
+        // int b;
+        // a=10;
+        // b=a;
+        // Console.WriteLine("valor de a:"+a);
+        // Console.WriteLine("valor de b:"+b);
+
+        //------- Ejercicio 1 ------
+        // int num=0;
+        // string? entrada, salida;
+        // Console.WriteLine("Ingrese un numero: ");
+        // entrada = Console.ReadLine();
+        // bool resultado = int.TryParse(entrada, out num);
+        // if (resultado)
+        // {
+        //     Console.WriteLine("Numero: " + num);
+        //     if (num > 0)
+        //     {
+        //         salida = invertirNumero(num);
+        //         Console.WriteLine("Numero Invertido: " + salida);
+        //     }
+        // }
+
+    }
+    public static string invertirNumero(int num)
+    {
+        if (num < 10)
+        {
+            return num.ToString();
+        } else
+        {
+            return (num%10).ToString() + invertirNumero(num/10);
+        }
     }
 }
