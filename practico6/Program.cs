c282fea8fc61a85e47d478d8e3a4895ee585b2e2
LucianoCV01 <Lucianocosentino01@gmail.com>
2023-06-18 21:48:05 -0300

    Ejercicio 5, terminado

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,81 +1,30 @@
-﻿internal class Program
+﻿using System.Text.RegularExpressions;
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
 
-        //------ Ejercicios Propuestos ------
-        //Apartado 6
-        // Console.WriteLine("Hello, World!");
-        // int a;
-        // int b;
-        // a=10;
-        // b=a;
-        // Console.WriteLine("valor de a:"+a);
-        // Console.WriteLine("valor de b:"+b);
-
-        //------- Ejercicio 1 ------
-        // int num=0;
-        // string? entrada, salida;
-        // Console.WriteLine("Ingrese un numero: ");
-        // entrada = Console.ReadLine();
-        // bool resultado = int.TryParse(entrada, out num);
-        // if (resultado)
-        // {
-        //     Console.WriteLine("Numero: " + num);
-        //     if (num > 0)
-        //     {
-        //         salida = invertirNumero(num);
-        //         Console.WriteLine("Numero Invertido: " + salida);
-        //     }
-        // }
-
-        //------ Ejercicio 4 ------
-        string? cadena1;
+        string? cadena;
         do
         {
-            Console.Write("Ingrese un texto: ");
-            cadena1 = Console.ReadLine();
-        } while (cadena1 == null);
-        // // Obtener la longitud de la cadena y muestre por pantalla.
-        // int longitud;
-        // longitud = cadena1.Length;
-        // Console.WriteLine(cadena1);
-        // Console.WriteLine($"Longitud: {longitud}");
-        // A partir de una segunda cadena ingresada por el usuario, concatene ambas cadenas distintas.
-        string? cadena2;
-        do
-        {
-            Console.Write("Ingrese un texto: ");
-            cadena2 = Console.ReadLine();
-        } while (cadena2 == null);
-        Console.WriteLine(cadena2);
-        string cadenasConca = string.Concat(cadena1, " ", cadena2);
-        Console.WriteLine(cadenasConca);
-        // // Extraer una subcadena de la cadena ingresada.
-        // string subcadena = cadena1.Substring(0, 4);
-        // Console.WriteLine(subcadena);
-        // // Recorrer la cadena de texto con un ciclo Foreach e ir mostrando elemento por elemento en pantalla
-        // foreach (var item in subcadena)
-        // {
-        //     Console.WriteLine(item);
-        // }
-        // // Buscar la ocurrencia de una palabra determinada en la cadena ingresada
-        // int ocurrencia = cadena1.IndexOf(cadena2);
-        // Console.WriteLine(cadena1[ocurrencia]);
-        // // Convierta la cadena a mayúsculas y luego a minúsculas.
-        // cadena1 = cadena1.ToUpper();
-        // cadena2 = cadena1.ToLower();
-        // Console.WriteLine(cadena1);
-        // Console.WriteLine(cadena2);
-        // // Ingrese una cadena separada por caracteres que usted determine y muestre por pantalla los resultados (Revisar el comportamiento de split())
-        // string[] cadenas = cadena1.Split(" ");
-        // foreach (var subcadena in cadenas)
-        // {
-        //     Console.WriteLine(subcadena);
-        // }
-        
+            Console.Write("Ingrese una cadena: ");
+            cadena = Console.ReadLine();
+        } while (cadena == null);
 
+        if (EsDireccionWeb(cadena))
+        {
+            Console.WriteLine("Es una direccion web");
+        } else
+        {
+            if (Esmail(cadena))
+            {
+                Console.WriteLine("Es un mail");
+            } else
+            {
+                Console.WriteLine("No es ninguno");
+            }
+        }
     }
     public static string invertirNumero(int num)
     {
@@ -86,5 +35,15 @@
         {
             return (num%10).ToString() + invertirNumero(num/10);
         }
+    }
+    public static bool EsDireccionWeb(string cadena)
+    {
+        string expresionRegular = @"^(https?://)?(w{3}.)?[a-zA-Z0-9.-]+.(com|com.ar)$";
+        return Regex.IsMatch(cadena, expresionRegular);
+    }
+    public static bool Esmail(string cadena)
+    {
+        string expresionRegular = @"^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$";
+        return Regex.IsMatch(cadena, expresionRegular);
     }
 }
--- readme.md
+++ readme.md
@@ -1,17 +1,10 @@
-# Ejercicio 4
-- ¿String es una tipo por valor o un tipo por referencia?
-String es un tipo por referencia, ósea que cuando asignas una cadena a una variable, en realidad estás almacenando una referencia a esa cadena en la memoria, en lugar de los propios datos de la cadena.
-- ¿Qué secuencias de escape tiene el tipo string?
-Secuencias de escape: 
-    - \'	Comilla simple
-    - \"	Comilla doble	
-    - \\	Barra diagonal inversa	
-    - \0	Null	
-    - \a	Alerta	 
-    - \b	Retroceso	
-    - \f	Avance de página	
-    - \n	Nueva línea	 
-    - \r	Retorno de carro
-- ¿Qué sucede cuando utiliza el carácter @ y $ antes de una cadena de texto?
-Cuando se utiliza el carácter "@" antes de una cadena de texto, la cadena se interpreta tal como está escrita, sin procesar secuencias de escape. 
-Por otro lado, cuando se utiliza el carácter "$" antes de una cadena de texto, este permite la incrustación de expresiones dentro de la cadena utilizando la sintaxis ${expresión}.
+# Ejercicio 5
+- ¿Funcionan únicamente en C#?
+Las expresiones regulares no están limitadas exclusivamente a C#, sino que son utilizadas en muchos lenguajes de programación y herramientas de procesamiento de texto. C# incluye soporte nativo para expresiones regulares a través del namespace System.Text.RegularExpressions.
+- ¿En qué casos le parecen útiles? Enuncie al menos 3.
+Algunos casos en los que las expresiones regulares son útiles incluyen:
+    - Validación de entrada: Las expresiones regulares pueden utilizarse para validar si una cadena de texto cumple con cierto formato o patrón específico. Por ejemplo, validar un correo electrónico, un número de teléfono o una dirección IP.
+    - Búsqueda y reemplazo de texto: Las expresiones regulares permiten buscar y reemplazar patrones específicos de texto en una cadena. Puedes realizar tareas como buscar todas las coincidencias de una palabra clave en un documento o reemplazar ciertos caracteres con otros.
+    - Extracción de información: Las expresiones regulares pueden utilizarse para extraer información específica de una cadena de texto. Por ejemplo, extraer todos los números de una cadena o extraer los nombres de usuario de una lista de correos electrónicos.
+- ¿Cómo se hace uso de estas en C#?
+En C#, se utiliza la clase Regex del namespace System.Text.RegularExpressions para trabajar con expresiones regulares. Esta clase proporciona métodos para compilar y ejecutar expresiones regulares, buscar coincidencias en cadenas de texto, realizar reemplazos y más.
